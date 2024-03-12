cb4680d7d30c7780443a11f3adc17a8671728b9e
YOGESWARAN KANNAN <yogeshgimmy07@gmail.com>
2024-03-12 16:10:34 +0530

    upto allocate space only

---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainContainer from './MainContainer';
+import Sidebar from './sidebar';
+
+const Body = () => {
+  return (
+    <div className='grid grid-flow-col'>
+      <Sidebar/>
+      <MainContainer/>
+    </div>
+  )
+}
+
+export default Body;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='p-2 col-span-1'>
+      
+    </div>
+  )
+}
+
+export default Sidebar;
